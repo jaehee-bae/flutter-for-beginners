92497683591e2dfb665152c89da7a4308bbd5d1c
jaehee-bae <hjaeb7777@gmail.com>
2023-10-09 15:11:37 +0900

    feat: #6.13 detail_screen에서 id값으로 상세정보, 에피소드 가져오기

--- toonflix/lib/screens/detail_screen.dart
+++ toonflix/lib/screens/detail_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/models/webtoon_detail_model.dart';
+import 'package:toonflix/models/webtoon_episode_model.dart';
+import 'package:toonflix/services/api_service.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   final String id, title, thumb;
   const DetailScreen({
     super.key,
@@ -8,6 +11,21 @@ class DetailScreen extends StatelessWidget {
     required this.title,
     required this.thumb,
   });
+
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  late Future<WebtoonDetailModel> webtoons;
+  late Future<List<WebtoonEpisodeModel>> episodes;
+
+  @override
+  void initState() {
+    super.initState();
+    webtoons = ApiService.getToonById(widget.id);
+    episodes = ApiService.getLatestEpisodesById(widget.id);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +37,7 @@ class DetailScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         foregroundColor: Colors.green.shade900,
         title: Text(
-          title,
+          widget.title,
           style: const TextStyle(
             fontSize: 24,
             fontWeight: FontWeight.w400,
@@ -34,7 +52,7 @@ class DetailScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Hero(
-              tag: id,
+              tag: widget.id,
               child: Container(
                 width: 200,
                 clipBehavior: Clip.hardEdge,
@@ -48,7 +66,7 @@ class DetailScreen extends StatelessWidget {
                       ),
                     ]),
                 child: Image.network(
-                  thumb,
+                  widget.thumb,
                 ),
               ),
             ),
