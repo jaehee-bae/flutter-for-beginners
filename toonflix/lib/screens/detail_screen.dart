a31c84aa06ed0ccd0f28d3fc1ca396d4696cc9bb
jaehee-bae <hjaeb7777@gmail.com>
2023-10-09 11:57:07 +0900

    feat: #6.9 웹툰 표지 클릭 시 detail_screen 화면으로 넘어가게 만들기

---
+++ toonflix/lib/screens/detail_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  final String id, title, thumb;
+  const DetailScreen({
+    super.key,
+    required this.id,
+    required this.title,
+    required this.thumb,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 2,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green.shade900,
+        title: Text(
+          title,
+          style: const TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+      ),
+      body: Column(children: [
+        const SizedBox(
+          height: 50,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 200,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 5,
+                      offset: const Offset(10, 10),
+                      color: Colors.black.withOpacity(0.3),
+                    ),
+                  ]),
+              child: Image.network(
+                thumb,
+              ),
+            ),
+          ],
+        ),
+      ]),
+    );
+  }
+}
--- toonflix/lib/widgets/webtoon_widget.dart
+++ toonflix/lib/widgets/webtoon_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/screens/detail_screen.dart';
 
 class Webtoon extends StatelessWidget {
   final String id, title, thumb;
@@ -12,34 +13,49 @@ class Webtoon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: 200,
-          clipBehavior: Clip.hardEdge,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: [
-                BoxShadow(
-                  blurRadius: 5,
-                  offset: const Offset(10, 10),
-                  color: Colors.black.withOpacity(0.3),
-                ),
-              ]),
-          child: Image.network(
-            thumb,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailScreen(
+              id: id,
+              title: title,
+              thumb: thumb,
+            ),
+            fullscreenDialog: true,
           ),
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        Text(
-          title,
-          style: const TextStyle(
-            fontSize: 22,
+        );
+      },
+      child: Column(
+        children: [
+          Container(
+            width: 200,
+            clipBehavior: Clip.hardEdge,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 5,
+                    offset: const Offset(10, 10),
+                    color: Colors.black.withOpacity(0.3),
+                  ),
+                ]),
+            child: Image.network(
+              thumb,
+            ),
           ),
-        ),
-      ],
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 22,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
